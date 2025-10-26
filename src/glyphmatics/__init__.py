1c9204da17a5ee2715c5f4a87703ea6b07327f9a
xxNine1Eightxx <phonegod918@gmail.com>
2025-10-25 20:14:58 -0500

    Update __init__.py

--- src/glyphmatics/__init__.py
+++ src/glyphmatics/__init__.py
@@ -1,10 +1,5 @@
-# src/glyphmatics/__init__.py
-from .component import UnifiedARCComponent
-from .visualization.network_graph import visualize_network
-
-__all__ = [
-    "UnifiedARCComponent",
-    "visualize_network",
-]
+from .component import GlyphMaticsEngine
+from .visualization.network import visualize_network
 
 __version__ = "0.1.0"
+__all__ = ["GlyphMaticsEngine", "visualize_network"]
