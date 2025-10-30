030dabe828b67bb83afdebe1c4538a99f7dfcb52
xxNine1Eightxx <phonegod918@gmail.com>
2025-10-30 06:12:48 -0500

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+# docs/conf.py
+import os
+import sys
+from datetime import datetime
+
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'GlyphMatics'
+copyright = f'{datetime.now().year}, xxNine1Eightxx'
+author = 'xxNine1Eightxx'
+release = '0.1.0'
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_title = "GlyphMatics Documentation"
